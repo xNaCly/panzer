3ade5a71a46a565dedc0c241e57dd8d3b9df88c6
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-09-18 13:58:26 +0200

    feat(lang#tokens): Token.Containspattern
    This new Token structure field allows for checking if the given identifier contains a pattern the core/shell/expansion should match against the files in the current directory

--- core/lang/tokens/token.go
+++ core/lang/tokens/token.go
@@ -3,7 +3,8 @@ package tokens
 type TokenType uint
 
 type Token struct {
-	Pos  int
-	Type TokenType
-	Raw  string
+	Pos             int
+	Type            TokenType
+	Raw             string
+	ContainsPattern bool
 }
