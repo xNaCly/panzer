a20f9d56e4124f0e067294e4723c968fb836c1a5
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-09-18 13:55:35 +0200

    feat(keyword#env): panic if more than 0 arguments

--- core/lang/keywords/env.go
+++ core/lang/keywords/env.go
@@ -5,7 +5,11 @@ import (
 	"os"
 )
 
+// Iterates over env variables and prints them
 func Env(args ...string) {
+	if len(args) > 0 {
+		panic("env: too many arguments")
+	}
 	for _, e := range os.Environ() {
 		fmt.Println(e)
 	}
